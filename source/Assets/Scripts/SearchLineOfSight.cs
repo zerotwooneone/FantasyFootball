184ac5c12cfde5581f3beb19d0b68be5f7b1f662
David Squire <squiredd@gmail.com>
2021-06-13 14:45:12 -0700

    Bully charges

--- source/Assets/Scripts/SearchLineOfSight.cs
+++ source/Assets/Scripts/SearchLineOfSight.cs
@@ -8,11 +8,13 @@ public class SearchLineOfSight : SearchStrategy
 {
     public GameObject Player;
     public Transform Self;
+    private Vector2 selfSize;
+    public float MaxDistance = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        selfSize = (GetComponent<CapsuleCollider2D>() as CapsuleCollider2D).size;
     }
 
     // Update is called once per frame
@@ -25,27 +27,23 @@ public class SearchLineOfSight : SearchStrategy
     {
         var rayDirection = (Player.transform.position - Self.position);
         Color rayColor;
-        var hit = Physics2D.Raycast(Self.position, rayDirection);
+        var hit = Physics2D.CapsuleCast(
+            Self.position, 
+            selfSize, 
+            CapsuleDirection2D.Vertical,
+            0f, 
+            rayDirection,
+            MaxDistance);
         if(hit.transform == Player.transform)
         {
             rayColor = Color.green;
-            Debug.Log(Player.transform.position);
+            return hit.transform.position;
         }
         else
         {
-            Debug.Log(hit.transform.position);
             rayColor = Color.red;
         }
         Debug.DrawRay(Self.position, rayDirection, rayColor);
-        // var hits = Physics2D.BoxCastAll(Self.transform.position, new Vector2(40, 40), rayDirection);
-        // if (hits.Length > 0)
-        // {
-        //     Debug.Log(hits.Select(h=>h.transform.ToString()));
-        // }
-        // else
-        // {
-        //     Debug.Log($"dir:{rayDirection} tar:{Player.transform.position} pos:{Self.position} hit:{hit.point}");
-        // }
 
         return null;
     }
