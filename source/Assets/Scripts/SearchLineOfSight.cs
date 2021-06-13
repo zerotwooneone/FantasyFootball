4cae543aa2dc353c1cff0757d0d8ac63065cf81f
David Squire <squiredd@gmail.com>
2021-06-13 14:08:04 -0700

    hit detection works

--- source/Assets/Scripts/SearchLineOfSight.cs
+++ source/Assets/Scripts/SearchLineOfSight.cs
@@ -24,16 +24,24 @@ public class SearchLineOfSight : SearchStrategy
     public override Vector2? FindTarget()
     {
         var rayDirection = (Player.transform.position - Self.position);
-        if (Physics.Raycast(Self.position, rayDirection, out var hit))
+        Color rayColor;
+        var hit = Physics2D.Raycast(Self.position, rayDirection);
+        if(hit.transform == Player.transform)
         {
+            rayColor = Color.green;
             Debug.Log(Player.transform.position);
         }
-        Debug.DrawRay(Self.position, rayDirection, Color.red, 2f);
-        var hits = Physics.BoxCastAll(Self.transform.position, new Vector3(40, 40, 1), rayDirection);
-        if (hits.Length > 0)
+        else
         {
-            Debug.Log(hits.Select(h=>h.transform.ToString()));
+            Debug.Log(hit.transform.position);
+            rayColor = Color.red;
         }
+        Debug.DrawRay(Self.position, rayDirection, rayColor);
+        // var hits = Physics2D.BoxCastAll(Self.transform.position, new Vector2(40, 40), rayDirection);
+        // if (hits.Length > 0)
+        // {
+        //     Debug.Log(hits.Select(h=>h.transform.ToString()));
+        // }
         // else
         // {
         //     Debug.Log($"dir:{rayDirection} tar:{Player.transform.position} pos:{Self.position} hit:{hit.point}");
