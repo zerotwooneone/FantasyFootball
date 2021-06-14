a0032268f985f5c1217722c397f16b6d6d5c8e00
David Squire <squiredd@gmail.com>
2021-06-13 19:28:16 -0700

    bully delays before charging

--- source/Assets/Scripts/SearchLineOfSight.cs
+++ source/Assets/Scripts/SearchLineOfSight.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class SearchLineOfSight : SearchStrategy
@@ -10,6 +11,8 @@ public class SearchLineOfSight : SearchStrategy
     public Transform Self;
     private Vector2 selfSize;
     public float MaxDistance = 20f;
+    public float ChargeDelaySeconds = 1f;
+    [CanBeNull] private Tuple<Vector2, float> lastSpotted;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +29,6 @@ public class SearchLineOfSight : SearchStrategy
     public override Vector2? FindTarget()
     {
         var rayDirection = (Player.transform.position - Self.position);
-        Color rayColor;
         var hit = Physics2D.CapsuleCast(
             Self.position, 
             selfSize, 
@@ -34,17 +36,36 @@ public class SearchLineOfSight : SearchStrategy
             0f, 
             rayDirection,
             MaxDistance);
-        if(hit.transform == Player.transform)
+
+        var current = Time.timeSinceLevelLoad;
+        var timeSinceLastSpotted = current - lastSpotted?.Item2;
+        var waitElapsed = timeSinceLastSpotted.HasValue && timeSinceLastSpotted.Value > ChargeDelaySeconds;
+        var spotted = hit.transform == Player.transform;
+        if(spotted)
         {
-            rayColor = Color.green;
-            return hit.transform.position;
+            Debug.Log($"spotted cur:{current} last:{lastSpotted?.Item2} elapsed:{timeSinceLastSpotted}");
+            var spotTime = lastSpotted?.Item2 ?? current;
+            lastSpotted = new Tuple<Vector2, float>(hit.transform.position, spotTime);
+            if (waitElapsed)
+            {
+                return hit.transform.position;
+            }
         }
         else
         {
-            rayColor = Color.red;
+            if (waitElapsed)
+            {
+                var position = lastSpotted != null
+                    ? lastSpotted.Item1
+                    : (Vector2?)null;
+                lastSpotted = null;
+                if (position.HasValue)
+                {
+                    var partialPosition = position.Value.normalized * 10;
+                    return partialPosition;
+                }
+            }
         }
-        Debug.DrawRay(Self.position, rayDirection, rayColor);
-
         return null;
     }
 }
