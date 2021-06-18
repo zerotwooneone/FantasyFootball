bc93840b620cf0c1930013e45600095824a96b14
David Squire <squiredd@gmail.com>
2021-06-17 22:22:37 -0700

    fixed typo

--- source/Assets/Scripts/ProjectileLauncher.cs
+++ source/Assets/Scripts/ProjectileLauncher.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ProjectileLauncher : MonoBehaviour
 {
     public Transform FirePoint;
     public Transform Target;
 
-    public GameObject Prefab;
+    public GameObject Fire1Prefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +26,7 @@ public class ProjectileLauncher : MonoBehaviour
 
     private void Shoot()
     {
-        var prefab = Instantiate(Prefab, FirePoint.position, FirePoint.rotation);
+        var prefab = Instantiate(Fire1Prefab, FirePoint.position, FirePoint.rotation);
         var projectileBehavior = prefab.GetComponent<ProjectileBehavior>();
         projectileBehavior.Target = Target;
     }
